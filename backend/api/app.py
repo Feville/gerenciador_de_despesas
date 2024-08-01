7222dca51efced956ce62385f22138e4807091b1
Marcos Dias <marvcosta04@gmail.com>
2024-08-01 17:47:18 -0300

    refactor: removendo create app

--- backend/api/app.py
+++ backend/api/app.py
@@ -5,13 +5,8 @@ Módulo de aplicação WEB Flask
 from flask import Flask
 from flask_cors import CORS
 from core.logs.logger import setup_logger
-from api.routes.access import access_blueprint
-from api.routes.finance import finance_blueprint
 
 logger = setup_logger(__name__)
 
 app = Flask(__name__)
 CORS(app)
-
-app.register_blueprint(access_blueprint)
-app.register_blueprint(finance_blueprint)
