9c5b86232dbb56f665fec150c9cb3995a2dbe772
Marcos Dias <marvcosta04@gmail.com>
2024-08-04 16:02:05 -0300

    temporario

--- frontend/finance/src/pages/home/login.jsx
+++ frontend/finance/src/pages/home/login.jsx
@@ -1,56 +1,59 @@
-import { useState } from "react"
-import { FaUser, FaRegAddressCard } from "react-icons/fa"
+import { useState } from "react";
+import { FaUser, FaRegAddressCard } from "react-icons/fa";
+import axios from "axios";
+import { API_BASE_URL } from "../../api/api";
 
-const login = () => {
-  const [username, setUsername] = useState("")
-  const [email, setEmail] = useState("")
-  
-  const handleSubmit = (event) => {
-    event.preventDefault()
+const Login = () => {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
 
-    console.log("Envio", username, email)
-  }
-  
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      const response = await axios.post(`${API_BASE_URL}/register`, { username, email });
+      console.log("Resposta do servidor:", response.data);
+    } catch (error) {
+      console.error("Erro ao enviar dados:", error);
+    }
+  };
+
   return (
     <div className='container'>
-      
       <form onSubmit={handleSubmit}>
-        
         <h1>Login</h1>
-      
+
         <div className="form">
           <FaUser className="icon"/>
-          <input 
-            type="text" 
-            placeholder='Nome de usuário' 
+          <input
+            type="text"
+            placeholder='Nome de usuário'
             id='txtBar'
-            onChange={(e) => setUsername(e.target.value)} 
-          />     
+            onChange={(e) => setUsername(e.target.value)}
+          />
         </div>
-       
+
         <div className="form">
-          <FaRegAddressCard className = "icon"/>
-          <input 
-            type="email" 
-            placeholder='Email' 
+          <FaRegAddressCard className="icon"/>
+          <input
+            type="email"
+            placeholder='Email'
             id='txtBar'
-            onChange={(e) => setEmail(e.target.value)} 
+            onChange={(e) => setEmail(e.target.value)}
           />
         </div>
 
         <div className="recall_forget">
           <label htmlFor="">
             <input type="checkbox" /> Remember me?
-            <a href="#"> Resgister </a>
+            <a href="#"> Register </a>
           </label>
         </div>
 
-        <button>Entrar</button>
-
-        </form>
-    
+        <button type="submit">Entrar</button>
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default login
+export default Login;
