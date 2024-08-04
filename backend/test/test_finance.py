8324e66c466a6200d6ca7f50ef9031a23796ca04
Marcos Dias <marvcosta04@gmail.com>
2024-08-04 11:27:11 -0300

    cleanup: comentários do código

--- backend/test/test_finance.py
+++ backend/test/test_finance.py
@@ -28,7 +28,6 @@ def setup_db():
 
 @pytest.fixture
 def mock_finance_controller(setup_db):
-    # Certifique-se de que o contexto da aplicação está ativo
     app = create_test_app()
     with app.app_context():
         return FinanceController(setup_db)
@@ -38,20 +37,14 @@ def test_get_user_balance_success(test_client, mock_finance_controller):
     email = "teste@email.com"
     category_name = "Comida"
 
-    # Obtém a resposta da função get_user_balance
     response = mock_finance_controller.get_user_balance(email)
 
-    # Se a função get_user_balance retornar uma Response, extraia o valor do conteúdo
     if isinstance(response, dict):
         initial_amount = response.get("Balance", 0)
     else:
-        # Trate casos onde response pode não ser um dicionário
         initial_amount = 0
 
-    additional_amount = (
-        50  # Este valor deve ser configurado de acordo com o cenário de teste
-    )
-    # Adiciona um saldo para o teste
+    additional_amount = 50
     mock_finance_controller.add_user_balance(email, additional_amount, category_name)
 
     expected_balance = initial_amount + additional_amount
