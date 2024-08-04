d078a7c3e19b512e5ca86b7af78673456153eaa2
Felipe <felipevnleme@hotmail.com>
2024-08-03 21:32:10 -0300

    feat: form login sem css

---
+++ frontend/finance/src/pages/home/login.jsx
@@ -0,0 +1,56 @@
+import { useState } from "react"
+import { FaUser, FaRegAddressCard } from "react-icons/fa"
+
+const login = () => {
+  const [username, setUsername] = useState("")
+  const [email, setEmail] = useState("")
+  
+  const handleSubmit = (event) => {
+    event.preventDefault()
+
+    console.log("Envio", username, email)
+  }
+  
+  return (
+    <div className='container'>
+      
+      <form onSubmit={handleSubmit}>
+        
+        <h1>Login</h1>
+      
+        <div className="form">
+          <FaUser className="icon"/>
+          <input 
+            type="text" 
+            placeholder='Nome de usuário' 
+            id='txtBar'
+            onChange={(e) => setUsername(e.target.value)} 
+          />     
+        </div>
+       
+        <div className="form">
+          <FaRegAddressCard className = "icon"/>
+          <input 
+            type="email" 
+            placeholder='Email' 
+            id='txtBar'
+            onChange={(e) => setEmail(e.target.value)} 
+          />
+        </div>
+
+        <div className="recall_forget">
+          <label htmlFor="">
+            <input type="checkbox" /> Remember me?
+            <a href="#"> Resgister </a>
+          </label>
+        </div>
+
+        <button>Entrar</button>
+
+        </form>
+    
+    </div>
+  )
+}
+
+export default login
