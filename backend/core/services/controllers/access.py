4ee5a11e0effb4214d6c8546731f61016de67d33
Vinnixz <marvcosta04@gmail.com>
2024-08-15 14:29:27 -0300

    fix: Corrigido bug de erro ao realizar login

--- backend/core/services/controllers/access.py
+++ backend/core/services/controllers/access.py
@@ -16,55 +16,52 @@ class AccessController:
 
     def _get_session(self):
         "Inicia a sessão do banco"
-        session = self.db_manager.Session()
-        return session
+        return self.db_manager.Session()
 
-    def hash_password(self, password: str):
-        "Coloca o hash na senha do usuário"
-        hashed_password = bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())
-        return hashed_password
+    def hash_password(self, password: str) -> str:
+        "Gera o hash da senha"
+        return bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())
 
-    def check_password(self, password: str, hashed_password: str):
+    def check_password(self, password: str, hashed_password: str) -> bool:
         "Verifica a senha do usuário comparando com o hash no banco"
-        if bcrypt.checkpw(password.encode("utf-8"), hashed_password):
-            return True
-        return False
+        return bcrypt.checkpw(password.encode("utf-8"), hashed_password)
 
-    def handle_commit_error(self, e: Exception) -> Response:
+    def handle_commit_error_user_exist(self) -> Response:
         "Erro de envio"
-        session = self._get_session()
-        session.rollback()
-        return (
-            jsonify({"msg": f"Problema ao registrar usuário, o erro {e} ocorreu"}),
-            400,
-        )
+        return jsonify({"msg": "Usuário já existe"}), 400
 
-    def build_user(self, username: str, email: str, hashed_password: str):
-        if (username or email or hashed_password) is None:
+    def build_user(self, username: str, email: str, hashed_password: bytes):
+        if not (username and email and hashed_password):
             return False
         return Users(username=username, email=email, secret_pass=hashed_password)
 
     def get_user_by_email(self, email: str) -> Users:
-        return self.db_manager.Session.query(Users).filter_by(email=email).first()
+        with self._get_session() as session:
+            return session.query(Users).filter_by(email=email).first()
 
     def create_user(self, username, email, password) -> Response:
         "Cria um novo usuário e registra YYYY-MM-DD e H:M:S"
         try:
             hashed_password = self.hash_password(password)
             new_user = self.build_user(username, email, hashed_password)
-            if new_user is False:
+            if not new_user:
                 return jsonify({"msg": "Parâmetros incompletos"}), 404
-            self._get_session().add(new_user)
-            self._get_session().commit()
+            with self._get_session() as session:
+                session.add(new_user)
+                session.commit()
             return jsonify({"msg": "Usuário registrado"}), 201
-        except Exception as e:
+        except Exception:
             session = self._get_session()
             session.rollback()
-            return self.handle_commit_error(e)
+            return self.handle_commit_error_user_exist()
 
     def login(self, email: str, password: str) -> Response:
         "Verifica o login do usuário"
-        user = self.get_user_by_email(email)
-        if user and self.check_password(password, user.secret_pass):
-            return jsonify({"msg": "Usuário logado", "email": email}), 200
-        return jsonify({"msg": "Problema ao logar usuário"}), 400
+        try:
+            user = self.get_user_by_email(email)
+            if user and self.check_password(password, user.secret_pass):
+                return jsonify({"msg": "Usuário logado", "email": email}), 200
+            else:
+                return jsonify({"msg": "Credenciais inválidas"}), 401
+        except Exception:
+            return jsonify({"msg": "Problema ao logar usuário:"}), 400
